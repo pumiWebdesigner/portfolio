d68b0c40a1c66487f6b04857537d45fba823026d
pumiWebdesigner <pumi.webdesigner@gmail.com>
2024-06-02 13:12:18 +0900

    高さ合わせ修正

--- public/assets/js/height-compare.js
+++ public/assets/js/height-compare.js
@@ -1,13 +1,15 @@
 {
-  const skillsContents = document.querySelector(".p-skills__contents");
-  const worksContents = document.querySelector(".p-works__contents");
+  if (window.innerWidth >= 768) {
+    const skillsContents = document.querySelector(".p-skills__contents");
+    const worksContents = document.querySelector(".p-works__contents");
 
-  const skillsHeight = skillsContents.offsetHeight;
-  const worksHeight = worksContents.offsetHeight;
+    const skillsHeight = skillsContents.offsetHeight;
+    const worksHeight = worksContents.offsetHeight;
 
-  if (skillsHeight > worksHeight) {
-    worksContents.style.height = skillsHeight + "px";
-  } else {
-    skillsContents.style.height = worksHeight + "px";
+    if (skillsHeight > worksHeight) {
+      worksContents.style.height = skillsHeight + "px";
+    } else {
+      skillsContents.style.height = worksHeight + "px";
+    }
   }
 }
